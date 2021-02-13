882a340b8309c898711162c04f0897f57dee6cc5
drunkdream <drunkdream@qq.com>
2021-02-13 11:47:10 +0800

    Update

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -12,7 +12,7 @@ async def test_file():
         fp.write(b'1234567890')
     timestamp = time.time()
     file = workspace.File(file_path)
-    assert(abs(file.last_modify_time - timestamp) < 0.01)
+    assert(abs(file.last_modify_time - timestamp) < 0.1)
     assert(file.size == 10)
     assert(file.hash == 'e807f1fcf82d132f9bb018ca6738a19f')
 
