e96e45fef70ea6dc993eccb1803cf973cfd24b61
drunkdream <drunkdream@qq.com>
2021-02-14 13:27:18 +0800

    Format by black

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -8,11 +8,10 @@ from wsterm import workspace
 
 async def test_file():
     file_path = tempfile.mkstemp()[1]
-    with open(file_path, 'wb') as fp:
-        fp.write(b'1234567890')
+    with open(file_path, "wb") as fp:
+        fp.write(b"1234567890")
     timestamp = time.time()
     file = workspace.File(file_path)
-    assert(abs(file.last_modify_time - timestamp) < 0.1)
-    assert(file.size == 10)
-    assert(file.hash == 'e807f1fcf82d132f9bb018ca6738a19f')
-
+    assert abs(file.last_modify_time - timestamp) < 0.1
+    assert file.size == 10
+    assert file.hash == "e807f1fcf82d132f9bb018ca6738a19f"
