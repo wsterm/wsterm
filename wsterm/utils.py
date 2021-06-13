5144544be735f349ce54fa9d401bfa63deb8b15a
drunkdream <drunkdream@qq.com>
2021-06-13 21:40:21 +0800

    Add win32 pty pipe

--- wsterm/utils.py
+++ wsterm/utils.py
@@ -10,6 +10,7 @@ import logging
 import os
 import subprocess
 import sys
+import time
 
 logger = logging.getLogger("wsterm")
 
@@ -185,7 +186,7 @@ class LineEditor(object):
             if self._buffer:
                 self._history.append(self._buffer)
                 self._history_index = 0
-            buffer = self._buffer + b"\n"
+            buffer = self._buffer + b"\r\n"
             self._buffer = b""
             self._cursor = 0
             if self._prev_cursor:
@@ -218,6 +219,284 @@ class LineEditor(object):
         sys.stdout.buffer.flush()
         self._prev_buffer = self._buffer
         self._prev_cursor = self._cursor
+
+
+class Win32ConsoleInputPipe(object):
+    """Win32 Console Input Pipe
+    """
+
+    def __init__(self):
+        import msvcrt
+        import win32api
+        import win32con
+        import win32console
+
+        stdin = win32console.GetStdHandle(win32console.STD_INPUT_HANDLE)
+        self._stdin = win32console.PyConsoleScreenBufferType(
+            win32api.DuplicateHandle(
+                win32api.GetCurrentProcess(),
+                stdin,
+                win32api.GetCurrentProcess(),
+                0,
+                0,
+                win32con.DUPLICATE_SAME_ACCESS,
+            )
+        )
+        stdin.Detach()
+        stdin.Close()
+        self._fd = msvcrt.open_osfhandle(int(self._stdin), os.O_APPEND)
+
+    def fileno(self):
+        return self._fd
+
+    def write(self, buffer):
+        import win32console
+
+        key_events = []
+        if isinstance(buffer, bytes):
+            buffer = buffer.decode()
+
+        for key in buffer:
+            event = win32console.PyINPUT_RECORDType(win32console.KEY_EVENT)
+            event.Char = key
+            event.KeyDown = True
+            event.RepeatCount = 1
+            key_events.append(event)
+        self._stdin.WriteConsoleInput(key_events)
+
+
+class Win32ConsoleOutputPipe(object):
+    """Console Output Pipe
+    """
+
+    def __init__(self, size):
+        import msvcrt
+        import win32console
+        import win32security
+
+        self._width, self._height = self._size = size
+        self._height *= 256
+        self._max_size = self._width * self._height
+        sa = win32security.SECURITY_ATTRIBUTES()
+        self._console = win32console.CreateConsoleScreenBuffer(SecurityAttributes=sa)
+        self._console.SetConsoleCursorPosition(win32console.PyCOORDType(0, 0))
+        self._console.SetConsoleScreenBufferSize(
+            win32console.PyCOORDType(self._width, self._height)
+        )
+        self._console.FillConsoleOutputCharacter(
+            "\x00", self._max_size, win32console.PyCOORDType(0, 0)
+        )
+        self._console.SetConsoleActiveScreenBuffer()
+        self._fd = msvcrt.open_osfhandle(int(self._console), os.O_APPEND)
+        self._running = True
+        self._buffer = []
+        self._last_read_pos = (0, 0)
+        safe_ensure_future(self.processing_screen_buffer())
+
+    def fileno(self):
+        return self._fd
+
+    def close(self):
+        self._running = False
+
+    def length(self, buffer):
+        result = 0
+        for c in buffer:
+            if ord(c) < 128:
+                result += 1
+            else:
+                result += 2
+        return result
+
+    def _merge_buffer(self, buffer):
+        buffer = buffer.encode("gbk")
+        if self._buffer and self.length(self._buffer[-1]) < self._width:
+            extra_size = self._width - self.length(self._buffer[-1])
+            self._buffer[-1] += buffer[:extra_size].decode("gbk")
+            buffer = buffer[extra_size:]
+        offset = 0
+        while offset < len(buffer):
+            self._buffer.append(buffer[offset : offset + self._width].decode("gbk"))
+            offset += self._width
+
+    def _handle_line(self, line):
+        buffer = b""
+        for c in line:
+            if c == "\x00":
+                buffer += b"\n"
+                return buffer
+            else:
+                buffer += c.encode()
+        return buffer
+
+    async def processing_screen_buffer(self):
+        import win32console
+
+        last_pos = (0, 0)
+        while self._running:
+            csbi = self._console.GetConsoleScreenBufferInfo()
+            if (
+                csbi["CursorPosition"].X != last_pos[0]
+                or csbi["CursorPosition"].Y != last_pos[1]
+            ):
+                width = csbi["Size"].X
+                count = (
+                    (csbi["CursorPosition"].Y - last_pos[1]) * width
+                    + csbi["CursorPosition"].X
+                    - last_pos[0]
+                )
+
+                output = self._console.ReadConsoleOutputCharacter(
+                    count, win32console.PyCOORDType(*last_pos)
+                )
+
+                self._merge_buffer(output)
+                last_pos = (csbi["CursorPosition"].X, csbi["CursorPosition"].Y)
+                if last_pos[1] > self._size[1] * 128 and last_pos[0] == 0:
+                    csbi = self._console.GetConsoleScreenBufferInfo()
+                    if (
+                        csbi["CursorPosition"].X == last_pos[0]
+                        or csbi["CursorPosition"].Y == last_pos[1]
+                    ):
+                        # Ensure cursor not move
+                        self._console.SetConsoleCursorPosition(
+                            win32console.PyCOORDType(0, 0)
+                        )
+                        self._console.FillConsoleOutputCharacter(
+                            "\x00",
+                            self._width * last_pos[1],
+                            win32console.PyCOORDType(0, 0),
+                        )
+
+                        last_pos = (0, 0)
+
+            await asyncio.sleep(0.1)
+
+    async def read(self, size, timeout=None):
+        last_read_time = time.time()
+        while self._running:
+            if self._buffer and (
+                self._last_read_pos[1] < len(self._buffer) - 1
+                or self._last_read_pos[0] < len(self._buffer[-1])
+                and self._last_read_pos[1] == len(self._buffer) - 1
+            ):
+                buffer = b""
+                if (
+                    self._last_read_pos[0] > 0
+                    and len(self._buffer[self._last_read_pos[1]])
+                    > self._last_read_pos[0]
+                ):
+                    # Read last line
+                    buffer += self._handle_line(
+                        self._buffer[self._last_read_pos[1]][self._last_read_pos[0] :]
+                    )
+                    self._last_read_pos = (0, self._last_read_pos[1] + 1)
+                for line in self._buffer[self._last_read_pos[1] :]:
+                    buffer += self._handle_line(line)
+
+                if buffer[-1] != 10:
+                    self._last_read_pos = len(self._buffer[-1]), len(self._buffer) - 1
+                else:
+                    self._last_read_pos = 0, len(self._buffer)
+                return buffer
+            if timeout and time.time() - last_read_time >= timeout:
+                return b""
+            await asyncio.sleep(0.1)
+
+
+class Win32NamedPipe(object):
+    def __init__(self, pipe_name, server_side=False):
+        self._pipe_name = r"\\.\pipe\%s" % pipe_name
+        self._pipe = None
+
+    async def listen(self, timeout=None):
+        import _winapi
+
+        flags = _winapi.PIPE_ACCESS_INBOUND | _winapi.FILE_FLAG_OVERLAPPED
+        self._pipe = _winapi.CreateNamedPipe(
+            self._pipe_name,
+            flags,
+            _winapi.PIPE_TYPE_MESSAGE
+            | _winapi.PIPE_READMODE_MESSAGE
+            | _winapi.PIPE_WAIT,
+            _winapi.PIPE_UNLIMITED_INSTANCES,
+            65536,
+            65536,
+            _winapi.NMPWAIT_WAIT_FOREVER,
+            _winapi.NULL,
+        )
+
+        ov = _winapi.ConnectNamedPipe(self._pipe, overlapped=True)
+        time0 = time.time()
+        while not timeout or time.time() - time0 < timeout:
+            _, ret = ov.GetOverlappedResult(False)
+            if not ret:
+                return
+            await asyncio.sleep(0.1)
+        else:
+            raise RuntimeError("Wait for pipe %s client timeout" % self._pipe_name)
+
+    async def connect(self, timeout=None):
+        import _winapi
+
+        time0 = time.time()
+        while not timeout or time.time() - time0 < timeout:
+            try:
+                self._pipe = _winapi.CreateFile(
+                    self._pipe_name,
+                    _winapi.GENERIC_WRITE,
+                    0,
+                    _winapi.NULL,
+                    _winapi.OPEN_EXISTING,
+                    _winapi.FILE_FLAG_OVERLAPPED,
+                    _winapi.NULL,
+                )
+            except FileNotFoundError:
+                await asyncio.sleep(0.1)
+            else:
+                break
+        else:
+            raise RuntimeError("Connect pipe %s timeout" % self._pipe_name)
+
+    async def read(self, size):
+        import _overlapped
+
+        ov = _overlapped.Overlapped(0)
+        ov.ReadFile(self._pipe, size)
+        while True:
+            try:
+                return ov.getresult()
+            except OSError as ex:
+                if ex.args[3] == 996:
+                    await asyncio.sleep(0.01)
+                else:
+                    raise ex
+
+    def write(self, buffer):
+        import _overlapped
+
+        ov = _overlapped.Overlapped(0)
+        ov.WriteFile(self._pipe, buffer)
+
+
+def dup_stdin_handle(target_process):
+    import win32api
+    import win32con
+    import win32console
+
+    stdin = win32api.GetStdHandle(win32console.STD_INPUT_HANDLE)
+    target_process_handle = win32api.OpenProcess(
+        win32con.PROCESS_ALL_ACCESS, False, target_process
+    )
+    handle = win32api.DuplicateHandle(
+        win32api.GetCurrentProcess(),
+        stdin,
+        target_process_handle,
+        0,
+        0,
+        win32con.DUPLICATE_SAME_ACCESS,
+    )
+    return handle
 
 
 def safe_ensure_future(coro, loop=None):
